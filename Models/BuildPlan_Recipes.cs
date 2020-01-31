f5c4908a167dd87af334b3c565e8a6c012c8aa7f
David Lannin <lanninjr@gmail.com>
2020-01-31 13:58:58 -0600

    Simple MRP.  Let the debugging begin.  Pushing DB change with this.

---
+++ Models/BuildPlan_Recipes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Recitopia.Models
+{
+    public partial class BuildPlan_Recipes
+    {
+        public int Id { get; set; }
+        public int Recipe_Id { get; set; }
+        public int BuildPlan_Id { get; set; }
+
+        [Display(Name = "Amount")]
+        [Column(TypeName = "decimal(18,0)")]
+        public decimal Amount { get; set; }        
+        public string Customer_Guid { get; set; }
+
+        public string Recipe_Name { get; set; }
+
+
+        public virtual BuildPlan BuildPlan { get; set; }
+        public virtual Recipe Recipe { get; set; }
+
+
+    }
+}
